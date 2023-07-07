8a91c87b7a55e507c2e013da159383bf6394f84c
Martossy Alex <github@mail.martossy.hu>
2023-07-07 14:10:00 +0200

    client and server see each other

--- NewsletterSignup/ClientApp/proxy.conf.js
+++ NewsletterSignup/ClientApp/proxy.conf.js
@@ -2,17 +2,19 @@ const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:54269';
-
+console.log(target);
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/api",
    ],
     target: target,
     secure: false,
     headers: {
       Connection: 'Keep-Alive'
-    }
+    },
+    ws: true,
+    changeOrigin: true,
   }
 ]
 
---
+++ NewsletterSignup/Controllers/NewsletterController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewsletterSignup.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NewsletterController: ControllerBase
+{
+    [HttpGet("test")]
+    [Produces(MediaTypeNames.Text.Plain)]
+    public IActionResult Test()
+    {
+        return new JsonResult(new {text = "Testing"});
+    }
+}
