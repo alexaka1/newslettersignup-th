12b7dc05ac09f6b23860c8ae8142458381ca332d
Martossy Alex <github@mail.martossy.hu>
2023-07-07 18:13:47 +0200

    some styling

--- NewsletterSignup/ClientApp/src/app/app.component.ts
+++ NewsletterSignup/ClientApp/src/app/app.component.ts
@@ -5,5 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'app';
+  // public static readonly darkThemeClassName = 'app-dark-mode';
+  // theme: 'light' | 'dark' = 'light';
+  // private readonly renderer: Renderer2;
+  //
+  // constructor(rendererFactory: RendererFactory2, private overlay: OverlayContainer) {
+  //   this.renderer = rendererFactory.createRenderer(null, null);
+  //   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  //     this.theme = 'dark';
+  //   }
+  //   this.renderer[this.theme === 'dark' ? 'addClass' : 'removeClass'](
+  //     document.querySelector('body'),
+  //     AppComponent.darkThemeClassName
+  //   );
+  //   if (this.theme === 'dark') {
+  //     this.overlay.getContainerElement().classList.add(AppComponent.darkThemeClassName);
+  //   } else {
+  //     this.overlay.getContainerElement().classList.remove(AppComponent.darkThemeClassName);
+  //   }
+  // }
 }
--- NewsletterSignup/ClientApp/src/app/app.module.ts
+++ NewsletterSignup/ClientApp/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
 import { TextFieldModule } from '@angular/cdk/text-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -29,6 +30,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatRadioModule,
     TextFieldModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
