e8cd16c47a8a1dc9e2a4ff84a99aeb1af37513a5
Martossy Alex <github@mail.martossy.hu>
2023-07-08 07:38:44 +0200

    list of valid email addresses that are not worth writing regexes for

--- NewsletterSignup/Newsletter/NewsletterSignupDto.cs
+++ NewsletterSignup/Newsletter/NewsletterSignupDto.cs
@@ -22,10 +22,17 @@ public class NewsletterSignupDto : IValidatableObject
         int? at = Email?.IndexOf('@');
 
         //  RFC 5322 and 5321 allow some crazy email addresses, so syntax validation besides @ character is not a good idea
-        // email can actually contain more than one @
         if (at is not > 0 || !(at.Value < Email?.Length - 1))
         {
             yield return new ValidationResult("Email is invalid", new[] { nameof(Email) });
         }
+        // list of valid email addresses
+        // iron.man@avengers.com
+        // spider-man@avengers.com
+        // t'challa@avengers.com
+        // rocket+groot@avengers.com
+        // "Bruce 'The Hulk' Banner"@avengers.com
+        // vision@[IPv6:2001:db8:1ff::a0b:dbd0]
+        // "wanda@vision"@avengers   // tld is also optional if it is an intranet
     }
 }
